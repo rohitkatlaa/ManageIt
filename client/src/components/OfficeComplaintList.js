206cc02ad8254b5d0c6c2f8444cb245453ed61fd
rohitkatlaa <rohitkatlaa@gmail.com>
2019-12-19 21:18:26 +0530

    real time updates with user condition

---
+++ client/src/components/OfficeComplaintList.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { connect } from 'react-redux';
+import { getItems, deleteItem , refreshItems} from '../actions/itemActions';
+import PropTypes from 'prop-types';
+import Pusher from 'pusher-js';
+import config from '../config/key'
+
+
+class OfficeComplaintList extends Component {
+  static propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired,
+    isAuthenticated: PropTypes.bool
+  };
+
+  componentDidMount() {
+    this.props.getItems();
+    Pusher.logToConsole = true;
+
+    var pusher = new Pusher(config.pusherAppId, {
+      cluster: 'ap2',
+      forceTLS: true
+    });
+
+    var channel = pusher.subscribe('ManageIt');
+    channel.bind('complainUpdate', (data)=> {
+      // alert(JSON.stringify(data));
+      this.props.refreshItems(data);
+    });
+  }
+
+  onDeleteClick = id => {
+    this.props.deleteItem(id);
+  };
+
+  
+
+
+  render() {
+    const { items } = this.props.item;
+    return (
+      <Container>
+        <ListGroup>
+          <TransitionGroup className='complain-list'>
+            {items.map(({ _id, name }) => (
+              <CSSTransition key={_id} timeout={500} classNames='fade'>
+                <ListGroupItem>
+                  {this.props.isAuthenticated ? (
+                    <Button
+                      className='remove-btn'
+                      color='danger'
+                      size='sm'
+                      onClick={this.onDeleteClick.bind(this, _id)}
+                    >
+                      &times;
+                    </Button>
+                  ) : null}
+                  {name}
+                </ListGroupItem>
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </ListGroup>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  item: state.item,
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(
+  mapStateToProps,
+  { getItems, deleteItem, refreshItems }
+)(OfficeComplaintList);
---
+++ client/src/components/StudentComplaintList.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { connect } from 'react-redux';
+import { getItems, deleteItem , refreshItems} from '../actions/itemActions';
+import PropTypes from 'prop-types';
+import Pusher from 'pusher-js';
+import config from '../config/key'
+
+
+class StudentComplaintList extends Component {
+  static propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired,
+    isAuthenticated: PropTypes.bool
+  };
+
+  componentDidMount() {
+    this.props.getItems();
+  }
+
+  onDeleteClick = id => {
+    this.props.deleteItem(id);
+  };
+
+  
+
+
+  render() {
+    const { items } = this.props.item;
+    return (
+      <Container>
+        <ListGroup>
+          <TransitionGroup className='complain-list'>
+            {items.map(({ _id, name }) => (
+              <CSSTransition key={_id} timeout={500} classNames='fade'>
+                <ListGroupItem>
+                  {this.props.isAuthenticated ? (
+                    <Button
+                      className='remove-btn'
+                      color='danger'
+                      size='sm'
+                      onClick={this.onDeleteClick.bind(this, _id)}
+                    >
+                      &times;
+                    </Button>
+                  ) : null}
+                  {name}
+                </ListGroupItem>
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </ListGroup>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  item: state.item,
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(
+  mapStateToProps,
+  { getItems, deleteItem, refreshItems }
+)(StudentComplaintList);
