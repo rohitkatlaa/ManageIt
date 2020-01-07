00ca76c7afdfa8e2baf264efb652ffa50625d3d5
rohitkatlaa <rohitkatlaa@gmail.com>
2020-01-08 00:00:02 +0530

    heroku testing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const mongoose=require('mongoose');
+const path = require('path');
 const config = require('config');
 
 
@@ -27,7 +28,16 @@ app.use('/api/complains',require('./routes/api/complain'));
 app.use('/api/users',require('./routes/api/users'));
 app.use('/api/auth',require('./routes/api/auth'));
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-const port =5000;
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
 app.listen(port,()=>console.log(`Server started on port ${port}`));
 
