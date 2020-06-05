7998228bd54062c96116391b18176751084adc60
Ishan Shanware <Ishan.Shanware@iiitb.org>
2020-06-05 16:22:12 +0530

    added msgs for reset

--- routes/api/reset.js
+++ routes/api/reset.js
@@ -28,10 +28,12 @@ router.post('/', auth, (req, res) => {
               var query = { _id: req.user.id };
               var reset = { $set: {password: hash} };
               User.findOneAndUpdate( query, reset, {new: true})
-                  .then(user => res.json(user));
+                  .then(user => res.json({msg: "Password Updated Successfully!"}));
             })
           })
-        };
+        }else{
+          res.json({msg: "Passwords Don't Match"})
+        }
       });
 });
 
