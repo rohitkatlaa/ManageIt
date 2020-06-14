feecab2d4b5a619d4e16fb47d3d4d2ea0edcb451
Ishan Shanware <Ishan.Shanware@iiitb.org>
2020-06-15 02:07:26 +0530

    added new green success alert to first page

--- client/src/containers/FirstPage.jsx
+++ client/src/containers/FirstPage.jsx
@@ -7,29 +7,66 @@ import { Container } from 'reactstrap';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { clearSuccess } from '../actions/authActions';
+import { Alert } from 'reactstrap';
 
 class FirstPage extends Component {
+
+  state = {
+    msg: null,
+    greenAlert: false
+  }
 
   static propTypes = {
     isAuthenticated: PropTypes.bool.isRequired,
     success: PropTypes.bool,
-    msg: PropTypes.string
   };
 
-  componentDidUpdate(prevProps) {
-    const { success, msg } = this.props;
+  setSuccessTimer = () => {
+    setTimeout(() => this.setState({
+      msg: null,
+      greenAlert: false
+    }), 2000);
   }
 
+  componentDidUpdate(prevProps) {
+    const { success } = this.props;
+
+    if(success == true){
+      this.setState({
+        msg: 'Password Reset Successfully!!',
+        greenAlert: true
+      })
+      this.setSuccessTimer();
+    }
+
+  }
+
+  onDismiss = () => {
+    this.setState({
+      greenAlert:false,
+      msg:null
+    });
+  }
 
   render() {
+    const resetSuccess = (
+      <Alert color="success" style={{position:"relative"}} isOpen={this.state.greenAlert} toggle={this.onDismiss}>
+          {this.state.msg}
+      </Alert>
+    );
     return (
       <div>
         <Container>
+          {
+          this.state.greenAlert ? resetSuccess : null
+          }
           <AppNavbar />
           <FilterModal />
           <ItemModal />
           <ComplaintList />
+          
         </Container>
+        
       </div>
     );
   }
@@ -38,7 +75,6 @@ class FirstPage extends Component {
 const mapStateToProps = state => ({
   isAuthenticated: state.auth.isAutenticated,
   success: state.auth.success,
-  msg: state.auth.msg
 });
 
 export default connect(mapStateToProps, { clearSuccess })(FirstPage);
